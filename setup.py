0789baf18c98f54d3245f8e6a7ae151efba06b10
Jon Landis <jlandis@akamai.com>
2014-10-16 16:15:41 -0400

    update link to developer site

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='edgegrid-python', 
-    version='1.0.2', 
+    version='1.0.3', 
     description='{OPEN} client authentication protocol for python-requests',
     author='Jonathan Landis',
     author_email='jlandis@akamai.com',
