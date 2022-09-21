3cdce2b1661c4a16a6e22f6bb76058dc99b5168c
Tatiana Slonimskaia <tslonims@akamai.com>
2022-09-21 11:57:24 +0000

    DXE-1621 Updated release version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='edgegrid-python', 
-    version='1.3.0',
+    version='1.3.1',
     description='{OPEN} client authentication protocol for python-requests',
     author='Jonathan Landis',
     author_email='jlandis@akamai.com',
